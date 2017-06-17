9a3fb0fe3793ef26b86574949bb9bce11bf25f61
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-06-17 11:41:59 +0300

    Manual String Exercises

---
+++ ManualString_Exer/Extract-Hyperlinks/ExtractHyperlinks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ExtractHyperlinks
+{
+    static void Main()
+    {
+        string line = string.Empty;
+        StringBuilder html = new StringBuilder();
+
+        while ((line = Console.ReadLine()) != "END")
+        {
+            html.Append(line);
+        }
+
+        Regex aPattern = new Regex(@"<a.+?>");
+        Regex hrefPattern = new Regex(@"href\s*=\s*((?<quotes>\""|')(?<res1>.+?)\k<quotes>|(?<res2>[^\""'][^\s>]+))");
+        var matchedTags = aPattern.Matches(html.ToString());
+
+        foreach (Match tag in matchedTags)
+        {
+            if (!hrefPattern.IsMatch(tag.Value))
+            {
+                continue;
+            }
+
+            var href = hrefPattern.Match(tag.Value);
+            var firstResult = href.Groups["res1"];
+            var secondResult = href.Groups["res2"];
+            Console.WriteLine(firstResult.Length == 0 ? secondResult : firstResult);
+        }
+    }
+}
---
+++ ManualString_Exer/Magic-Exchangeable-Words/MagicExchangeableWords.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MagicExchangeableWords
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        var firstString = input[0];
+        var secondString = input[1];
+        var areExchangeable = true;
+        var shorter = string.Empty;
+        var longer = string.Empty;
+        var dictionary = new Dictionary<char, char>();
+
+        if (firstString.Length <= secondString.Length)
+        {
+            shorter = firstString;
+            longer = secondString;
+        }
+        else
+        {
+            shorter = secondString;
+            longer = firstString;
+        }
+
+        var remainingString = longer.Substring(shorter.Length);
+
+        for (int i = 0; i < shorter.Length; i++)
+        {
+            if (dictionary.ContainsKey(firstString[i]))
+            {
+                var value = dictionary[firstString[i]];
+
+                if (value != secondString[i])
+                {
+                    areExchangeable = false;
+                    break;
+                }
+            }
+            else
+            {
+                if (dictionary.ContainsValue(secondString[i]))
+                {
+                    areExchangeable = false;
+                    break;
+                }
+                else
+                {
+                    dictionary.Add(firstString[i], secondString[i]);
+                }
+            }
+        }
+
+        foreach (var letter in remainingString)
+        {
+            if (!dictionary.ContainsKey(letter) && !dictionary.ContainsValue(letter))
+            {
+                areExchangeable = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(areExchangeable ? "true" : "false");
+    }
+}
