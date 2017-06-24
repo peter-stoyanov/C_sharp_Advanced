5af8a0168fe1e0a024cf1b612f92da048091c606
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-06-24 07:04:40 +0300

    exam prep on exam from 22Aug2016

---
+++ ExamPrep_22Aug2016/AshesRoses/AshesRoses.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AshesRoses
+{
+    class AshesRoses
+    {
+        static void Main(string[] args)
+        {
+            //Grow <Dorne> <Indigo> 2000
+
+            var input = string.Empty;
+            var regions = new Dictionary<string, Dictionary<string, int>>();
+
+            string pattern = @"^Grow <([A-Z][a-z]+)> <([a-zA-Z0-9]+)> ([0-9]{1,10})$";
+            Regex rgx = new Regex(pattern);
+
+
+            while ((input = Console.ReadLine()) != "Icarus, Ignite!")
+            {
+                if (rgx.Match(input).Success)
+                {
+                    var match = rgx.Match(input);
+                    var region = match.Groups[1].Value;
+                    var color = match.Groups[2].Value;
+                    var roses = int.Parse(match.Groups[3].Value);
+
+                    if (regions.ContainsKey(region))
+                    {
+                        if (regions[region].ContainsKey(color))
+                        {
+                            regions[region][color] += roses;
+                        }
+                        else
+                        {
+                            regions[region].Add(color, roses);
+                        }
+                    }
+                    else
+                    {
+                        var newRoses = new Dictionary<string, int>();
+                        newRoses.Add(color, roses);
+                        regions.Add(region, newRoses);
+                    }
+                }
+            }
+
+            var orderedRegions = regions
+                .OrderByDescending(pair => pair.Value.Values.Sum())
+                .ThenBy(pair => pair.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var pair in orderedRegions)
+            {
+                var region = pair.Key;
+                var colors = pair.Value;
+                var orderedColors = colors
+                    .OrderBy(x => x.Value)
+                    .ThenBy(x => x.Key)
+                    .ToDictionary(x => x.Key, x => x.Value);
+
+
+                Console.WriteLine($"{region}");
+
+                foreach (var nestedPair in orderedColors)
+                {
+                    var color = nestedPair.Key;
+                    var roseAmount = nestedPair.Value;
+                    Console.WriteLine($"*--{color} | {roseAmount}");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ExamPrep_22Aug2016/NMS/NMS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMS
+{
+    class NMS
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string line;
+            while ((line = Console.ReadLine()) != "---NMS SEND---")
+            {
+                sb.Append(line);
+
+            }
+
+            string delimiter = Console.ReadLine();
+
+            char previous = '\0';
+            int counter = 0;
+            foreach (var letter in sb.ToString().ToLower().ToCharArray())
+            {
+                if (previous != '\0' && (int)letter < (int)previous)
+                {
+                    sb.Insert(counter, delimiter);
+                    previous = letter;
+                    counter = counter + delimiter.Length + 1;
+                }
+                else
+                {
+                    previous = letter;
+                    counter++;
+                }
+
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
